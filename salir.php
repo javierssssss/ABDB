83ff98ef8a962b3267c7c7c44a2bfa1d637de8bf
Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-12-10 23:38:57 -0500

    salir

--- salir.php
+++ salir.php
@@ -3,9 +3,8 @@ $salir = $_POST['salir'];
 if(!empty($salir)){
     session_start();
     session_destroy();
-    header('Location: index.php');
+    header('Location: https://sql-injection.cleverapps.io ');
 }
-
 
 header('Location: index.php');
 ?>

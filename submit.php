ad248923e28a9cb701bcbb0218c4291ed848f6f6
Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-12-10 21:17:06 -0500

    PDO

--- submit.php
+++ submit.php
@@ -1,4 +1,13 @@
 <?php
-echo "Hola bienvenido!"
+ $bdd = new PDO(
+    "mysql:host=" . getenv("MYSQL_ADDON_HOST") . ";dbname=" . getenv("MYSQL_ADDON_DB"),
+    getenv("MYSQL_ADDON_USER"),
+    getenv("MYSQL_ADDON_PASSWORD")
+);
 
+// Check connection
+if ( $bdd ) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>

48d1c48d0d5365ea4ec45d8774c748a232f3d33c
Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-12-10 21:05:37 -0500

    Mensajes

--- index.php
+++ index.php
@@ -5,24 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <!-- Title Page -->  
     <title>Login</title>
- 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/styles.css"> 
+    <link rel="stylesheet" href="estilos.css"> 
 </head>
 <body>
     <div id="container">
-        <form>
+        <form method="POST" action="submit.php">
             <!-- Username -->
-            <label for="name">Username:</label>
-            <input type="name">
+            <label for="name">Usuario:</label>
+            <input type="name" name="usuario">
             <!-- Password -->
-            <label for="username">Password:</label>
-            <p><a href="#">Forgot your password?</a>
-            <input type="password">
+            <label for="username">Clave:</label>
+           
+            <input type="password" name="clave">
             <div id="lower">
-                <input type="checkbox"><label class="check" for="checkbox">Keep me logged in</label>
                 <!-- Submit Button -->
                 <input type="submit" value="Login">
             </div>
